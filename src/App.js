60d234407fe1c5a8d19949ab0e9341452ecb8d84
zksmith <zahiersmith96@gmail.com>
2019-02-20 19:43:37 -0500

    moved featured stream to own component

--- src/App.js
+++ src/App.js
@@ -1,69 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
+import Landing from "./components/Landing";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      currentStream: "",
-      allFeaturedStreams: ""
-    };
+    this.state = {};
   }
-  componentDidMount() {
-    var url =
-      "https://api.twitch.tv/kraken/streams/featured?client_id=b1r5nyhmnh17p0t2orohaquamw0f3h7";
-    let self = this;
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        console.log(data.featured[0].stream);
-        self.setState({
-          currentStream: data.featured[0].stream.channel.name,
-          allFeaturedStreams: data.featured
-        });
-      })
-      .catch(error => console.log(error));
-  }
+
   render() {
-    if (this.state.currentStream !== "") {
-      return (
-        <div className="App">
-          <Sidebar />
-          <div className="main-content">
-            <div className="featured-streamer">
-              <div className="video">
-                <iframe
-                  src={`https://player.twitch.tv/?channel=${
-                    this.state.currentStream
-                  }&muted=true`}
-                  title={`featured streamer ${this.state.currentStream}`}
-                  frameBorder="0"
-                  height="740"
-                  width="416"
-                />
-              </div>
-            </div>
-            <div className="thumbnail-row">
-              {this.state.allFeaturedStreams.map(stream => (
-                <img src={stream.stream.preview.medium} />
-              ))}
-            </div>
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div className="App">
-          <Sidebar />
-          <div className="main-content">
-            <p>Loading...</p>
-          </div>
-        </div>
-      );
-    }
+    return (
+      <div className="App">
+        <Sidebar />
+        <Landing />
+      </div>
+    );
   }
 }
 
---
+++ src/components/Landing.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import "./Landing.css";
+
+class Landing extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentStream: "",
+      allFeaturedStreams: ""
+    };
+  }
+  componentDidMount() {
+    var url =
+      "https://api.twitch.tv/kraken/streams/featured?client_id=b1r5nyhmnh17p0t2orohaquamw0f3h7";
+    let self = this;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        console.log(data.featured[0].stream);
+        self.setState({
+          currentStream: data.featured[0].stream.channel.name,
+          allFeaturedStreams: data.featured
+        });
+      })
+      .catch(error => console.log(error));
+  }
+  render() {
+    if (this.state.currentStream !== "") {
+      return (
+        <div className="main-content">
+          <div className="featured-streamer">
+            <div className="video">
+              <iframe
+                src={`https://player.twitch.tv/?channel=${
+                  this.state.currentStream
+                }&muted=true`}
+                title={`featured streamer ${this.state.currentStream}`}
+                frameBorder="0"
+                height="740"
+                width="416"
+              />
+            </div>
+          </div>
+          <div className="thumbnail-row">
+            {this.state.allFeaturedStreams.map(stream => (
+              <img
+                src={stream.stream.preview.medium}
+                alt={stream.stream.channel.display_name}
+              />
+            ))}
+          </div>
+        </div>
+      );
+    } else {
+      return <p>Loading...</p>;
+    }
+  }
+}
+export default Landing;
