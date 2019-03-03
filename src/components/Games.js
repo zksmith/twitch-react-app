868f5b3dcd633ef2caf73bf3228031193962f95e
zksmith <zahiersmith96@gmail.com>
2019-03-03 16:32:59 -0500

    added base games page functionality

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,8 +1,45 @@
 import React, { Component } from "react";
+import { Link } from "@reach/router";
+import runtimeEnv from "@mars/heroku-js-runtime-env";
 
 class Games extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      allGames: []
+    };
+  }
+  componentDidMount() {
+    const env = runtimeEnv();
+    var url = `https://api.twitch.tv/kraken/games/top?limit=100&client_id=${
+      env.REACT_APP_CLIENT_ID
+    }`;
+    let self = this;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        self.setState({
+          allGames: data.top
+        });
+      })
+      .catch(error => console.log(error));
+  }
   render() {
-    return <p>Games</p>;
+    let state = this.state;
+    return (
+      <div className="games">
+        {state.allGames.map(game => (
+          <div className="game-card" key={game.game.name}>
+            <Link to={`/streams/${game.game.name}`}>
+              <img src={game.game.box.medium} />
+              <p>{game.game.name}</p>
+            </Link>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
