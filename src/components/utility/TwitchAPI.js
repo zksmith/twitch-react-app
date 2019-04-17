3f51e56a6d132937dffe3024caa5cb15a4093371
zksmith <zahiersmith96@gmail.com>
2019-04-17 19:42:28 -0400

    fixed getgames api error

--- src/components/utility/TwitchAPI.js
+++ src/components/utility/TwitchAPI.js
@@ -2,13 +2,14 @@ import runtimeEnv from "@mars/heroku-js-runtime-env";
 
 const ENV = runtimeEnv();
 
-const GAMESURL = `https://api.twitch.tv/kraken/games/top?limit=100`;
+const GAMESURL = `https://api.twitch.tv/kraken/games/top?limit=100&client_id=${
+  ENV.REACT_APP_CLIENT_ID}`;
 const ALLSTREAMSURL = `https://api.twitch.tv/kraken/streams?limit=100&client_id=${
   ENV.REACT_APP_CLIENT_ID
-}`;
+  }`;
 const FEATUREDSTREAMSURL = `https://api.twitch.tv/kraken/streams/featured?client_id=${
   ENV.REACT_APP_CLIENT_ID
-}`;
+  }`;
 
 const getFeaturedStreams = fetch(FEATUREDSTREAMSURL)
   .then(response => response.json())
@@ -34,7 +35,7 @@ const getAllStreams = fetch(ALLSTREAMSURL)
 const getStreamsForGame = game => {
   let url = `https://api.twitch.tv/kraken/search/streams?limit=100&query=${game}&client_id=${
     ENV.REACT_APP_CLIENT_ID
-  }`;
+    }`;
   return fetch(url)
     .then(response => response.json())
     .then(data => {
