c9713c154d557ae0d272fe5b1550a8b0d03c1910
zksmith <zahiersmith96@gmail.com>
2019-06-15 22:32:13 -0400

    changed streams.js to a function component

--- src/components/Streams.js
+++ src/components/Streams.js
@@ -1,67 +1,54 @@
-import React, { Component, Fragment } from "react";
+import React, { useEffect, useState, Fragment } from "react";
 import { getStreamsForGame } from "./utility/TwitchAPI";
 import "./Streams.css";
 import StreamCard from "./StreamCard";
 import Loading from "./Loading";
 
-class Streams extends Component {
-  state = { allStreams: [], loading: false };
+const Streams = ({ ["*"]: category }) => {
+  const [allStreams, setAllStreams] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  getGames = async category => {
-    this.setState({ loading: true });
+  const getStreams = async category => {
+    setLoading(true);
 
     const res = await getStreamsForGame(category);
-    this.setState({
-      allStreams: res,
-      loading: false
-    });
+    setAllStreams(res);
+    setLoading(false);
   };
 
-  componentDidMount() {
-    // this.props[*] gets the url param after /streams
-    this.getGames(this.props["*"]);
-  }
+  useEffect(() => {
+    getStreams(category);
+  }, [category]);
 
-  componentDidUpdate(prevProps) {
-    /* handling for user clicking the "Streams" navbar link
-     * while browsing streams for specific game */
-    if (prevProps["*"] !== this.props["*"]) {
-      this.getGames(this.props["*"]);
-    }
+  if (loading) {
+    return <Loading />;
+  } else if (allStreams) {
+    //added allStreams to if statement to handle twitch API error
+    return (
+      <section className="streams">
+        {/* conditional render handling successful call but no results */}
+        {allStreams.length > 0 ? (
+          <Fragment>
+            {allStreams.map(stream => (
+              <StreamCard
+                name={stream.channel.name}
+                preview={stream.preview.medium}
+                game={stream.game}
+                logo={stream.channel.logo}
+                status={stream.channel.status}
+                viewers={stream.viewers}
+                key={stream.channel.name}
+              />
+            ))}
+          </Fragment>
+        ) : (
+          <h1>Could not find any streams for "{category}"</h1>
+        )}
+      </section>
+    );
+  } else {
+    return <p>Twitch API is having troubles right now</p>;
   }
-
-  render() {
-    let { loading, allStreams } = this.state;
-    //added allStreams to if statement ta handle twitch API error
-    if (allStreams && !loading) {
-      return (
-        <section className="streams">
-          {/* conditional render handling successful call but no results */}
-          {allStreams.length > 0 ? (
-            <Fragment>
-              {allStreams.map(stream => (
-                <StreamCard
-                  name={stream.channel.name}
-                  preview={stream.preview.medium}
-                  game={stream.game}
-                  logo={stream.channel.logo}
-                  status={stream.channel.status}
-                  viewers={stream.viewers}
-                  key={stream.channel.name}
-                />
-              ))}
-            </Fragment>
-          ) : (
-            <h1>Could not find any streams for "{this.props["*"]}"</h1>
-          )}
-        </section>
-      );
-    } else if (loading) {
-      return <Loading />;
-    } else {
-      return <p>Twitch API is having troubles right now</p>;
-    }
-  }
-}
+};
 
 export default Streams;
