00fb622ec79de5788c8c1ab538c79feda9943749
zksmith <zahiersmith96@gmail.com>
2019-06-18 08:24:03 -0400

    added base chat and style to channel page

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -1,7 +1,25 @@
 import React from "react";
-import Video from "./Video";
+// import Video from "./Video";
 import "./Channel.css";
 
-const Channel = ({ channelName }) => <Video currentStream={channelName} />;
-
+const Channel = ({ channelName }) => {
+  return (
+    <div style={{ display: "flex", height: "100vh" }}>
+      <iframe
+        src={`https://player.twitch.tv/?channel=${channelName}&muted=true`}
+        title={`featured streamer ${channelName}`}
+        frameBorder="0"
+        style={{ width: "70%" }}
+      />
+      <iframe
+        frameBorder="0"
+        title={`featured streamer ${channelName} chat`}
+        scrolling="no"
+        id="chat_embed"
+        src={`https://www.twitch.tv/embed/${channelName}/chat`}
+        width="30%"
+      />
+    </div>
+  );
+};
 export default Channel;
