bdfb3e92d486a56cc4a621abac78c52eaeb4198f
zksmith <zahiersmith96@gmail.com>
2019-06-30 11:41:56 -0400

    added try catch to ajax calls

--- src/actions/twitchActions.js
+++ src/actions/twitchActions.js
@@ -15,32 +15,44 @@ const kraken = axios.create({
 
 // Get all featured streams - used in sidebar and homepage
 export const getFeaturedStreams = () => async dispatch => {
-  const response = await kraken.get("streams/featured");
+  try {
+    const response = await kraken.get("streams/featured");
 
-  dispatch({
-    type: GET_FEATURED_STREAMS,
-    payload: response.data.featured
-  });
+    dispatch({
+      type: GET_FEATURED_STREAMS,
+      payload: response.data.featured
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const getTopGames = () => async dispatch => {
-  dispatch(setLoading());
-  const response = await kraken.get("games/top?limit=100");
-  dispatch({
-    type: GET_TOP_GAMES,
-    payload: response.data.top
-  });
+  try {
+    dispatch(setLoading());
+    const response = await kraken.get("games/top?limit=100");
+    dispatch({
+      type: GET_TOP_GAMES,
+      payload: response.data.top
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const getStreamsByCategory = category => async dispatch => {
-  dispatch(setLoading());
-  const response = await kraken.get(
-    category ? `streams?limit=100&game=${category}` : `streams?limit=100`
-  );
-  dispatch({
-    type: GET_STREAMS_BY_CATEGORY,
-    payload: response.data.streams
-  });
+  try {
+    dispatch(setLoading());
+    const response = await kraken.get(
+      category ? `streams?limit=100&game=${category}` : `streams?limit=100`
+    );
+    dispatch({
+      type: GET_STREAMS_BY_CATEGORY,
+      payload: response.data.streams
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const setLoading = () => {
