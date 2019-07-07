e889d23764bf229dd7140e81e5fbbd7eda4a787e
zksmith <zahiersmith96@gmail.com>
2019-07-07 12:44:39 -0400

    removed unneeded initial state in redux sore

--- src/store.js
+++ src/store.js
@@ -3,13 +3,10 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-const initialState = {};
-
 const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
-  initialState,
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
