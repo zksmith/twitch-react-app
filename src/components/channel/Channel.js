75bf1d7099cacab9e0129d32245e217cd894d661
Zahier Smith <zahier_smith@apple.com>
2019-11-04 15:58:10 -0800

    updated channel page code

--- src/components/channel/Channel.js
+++ src/components/channel/Channel.js
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from "react";
-import { connect } from "react-redux";
-import { getChannelInfo } from "../../actions/twitchActions";
+import React, { useState } from "react";
 import "./Channel.css";
 
 const Channel = ({ channelName, getChannelInfo, channelInfo }) => {
@@ -15,39 +13,8 @@ const Channel = ({ channelName, getChannelInfo, channelInfo }) => {
     localStorage.setItem("chatVisible", !chatVisible);
     setChatVisibile(!chatVisible);
   };
-
-  useEffect(() => {
-    getChannelInfo(channelName);
-  }, [channelName, getChannelInfo]);
-
   return (
-    <>
-      {channelInfo && (
-        <>
-          <section className="channel-container">
-            <div className="info-stream">
-              <header className="channel-header">
-                <div className="header-block">
-                  <img
-                    src={channelInfo.logo}
-                    style={{ width: "30px", borderRadius: "50%" }}
-                    alt={`${channelInfo.name} logo`}
-                  />
-                  <span>{channelInfo.name}</span>
-                </div>
-                <div className="header-block">
-                  <a href="#!">Clips</a>
-                </div>
-                <div className="header-block">
-                  <a href="#!">Videos</a>
-                </div>
-                <div className="header-block">
-                  <span>
-                    <a href="#!">Followers:</a>{" "}
-                    {channelInfo.followers.toLocaleString()}
-                  </span>
-                </div>
-              </header>
+    <div className="channel-container">
               <iframe
                 className="video-frame"
                 src={`https://player.twitch.tv/?channel=${channelName}&muted=true`}
@@ -55,7 +22,6 @@ const Channel = ({ channelName, getChannelInfo, channelInfo }) => {
                 frameBorder="0"
                 allowFullScreen
               />
-            </div>
             <div className="chat-container">
               <button className="btn" onClick={toggleChat}>
                 {chatVisible ? "Hide Chat" : "Show Chat"}
@@ -70,18 +36,8 @@ const Channel = ({ channelName, getChannelInfo, channelInfo }) => {
                 />
               )}
             </div>
-          </section>
-        </>
-      )}
-    </>
+    </div>
   );
 };
 
-const mapStateToProps = state => ({
-  channelInfo: state.twitch.viewedChannel
-});
-
-export default connect(
-  mapStateToProps,
-  { getChannelInfo }
-)(Channel);
+export default Channel;
