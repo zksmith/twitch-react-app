c2b87240a3ff2240ec3f7a59a3b86974b1d5c798
zksmith <zahiersmith96@gmail.com>
2020-06-08 23:46:59 -0400

    access token

--- src/actions/twitchActions.js
+++ src/actions/twitchActions.js
@@ -6,50 +6,54 @@ import {
   SET_LOADING,
   SET_VIEWED_CHANNEL,
   GET_GAME_INFO,
-  SET_API_ERROR
+  SET_API_ERROR,
 } from "./types";
 
 const twitchClientId = process.env.REACT_APP_CLIENT_ID;
+const twitchAccessToken = process.env.REACT_APP_ACCESS_TOKEN;
 
 const helix = axios.create({
   baseURL: "https://api.twitch.tv/helix/",
-  headers: { "Client-ID": twitchClientId }
+  headers: {
+    "Client-ID": twitchClientId,
+    Authorization: `Bearer ${twitchAccessToken}`,
+  },
 });
 
-export const setAPIError = bool => {
+export const setAPIError = (bool) => {
   return {
     type: SET_API_ERROR,
-    payload: bool
+    payload: bool,
   };
 };
 
 // Get all featured streams - used in sidebar and homepage
-export const getFeaturedStreams = () => async dispatch => {
+export const getFeaturedStreams = () => async (dispatch) => {
   try {
     const response = await helix.get("streams");
     dispatch({
       type: GET_FEATURED_STREAMS,
-      payload: response.data.data
+      payload: response.data.data,
     });
   } catch (error) {
     dispatch(setAPIError(true));
   }
 };
 
-export const getTopGames = () => async dispatch => {
+export const getTopGames = () => async (dispatch) => {
   try {
     dispatch(setLoading());
     const response = await helix.get("games/top?first=100");
     dispatch({
       type: GET_TOP_GAMES,
-      payload: response.data.data
+      payload: response.data.data,
     });
   } catch (error) {
     dispatch(setAPIError(true));
   }
 };
 
-export const getStreams = gameID => async dispatch => {
+export const getStreams = (gameID) => async (dispatch) => {
   try {
     dispatch(setLoading());
     const response = await helix.get(
@@ -57,37 +61,37 @@ export const getStreams = gameID => async dispatch => {
     );
     dispatch({
       type: GET_STREAMS,
-      payload: response.data.data
+      payload: response.data.data,
     });
   } catch (error) {
     dispatch(setAPIError(true));
   }
 };
 
-export const getGameInfo = gameID => async dispatch => {
+export const getGameInfo = (gameID) => async (dispatch) => {
   try {
     const response = await helix.get(`games?id=${gameID}`);
     dispatch({
       type: GET_GAME_INFO,
       payload: {
-        [gameID]: response.data.data[0]
-      }
+        [gameID]: response.data.data[0],
+      },
     });
   } catch (error) {
     dispatch(setAPIError(true));
   }
 };
 
-export const getChannelInfo = channelName => async dispatch => {
+export const getChannelInfo = (channelName) => async (dispatch) => {
   const response = await helix.get(`channels/${channelName}`);
   dispatch({
     type: SET_VIEWED_CHANNEL,
-    payload: response.data
+    payload: response.data,
   });
 };
 
 export const setLoading = () => {
   return {
-    type: SET_LOADING
+    type: SET_LOADING,
   };
 };
