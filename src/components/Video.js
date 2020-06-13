3afab5b9ea91257ecaacc582b477c03f353673b2
zksmith <zahiersmith96@gmail.com>
2020-06-13 02:55:09 -0400

    twitch iframe update

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,7 +4,7 @@ import "./Video.css";
 const Video = ({ currentStream }) => (
   <div className="iframe-container">
     <iframe
-      src={`https://player.twitch.tv/?channel=${currentStream}&muted=true`}
+      src={`https://player.twitch.tv/?channel=${currentStream}&muted=true&parent=twitch-react-app.netlify.app`}
       title={`featured streamer ${currentStream}`}
       frameBorder="0"
       height="500px"
